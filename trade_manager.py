23cfd152b7df2d3def43f82bf645ff63b2c2e157
yunusemreyil <85075245+yunusemreyil@users.noreply.github.com>
2025-05-04 14:35:30 +0300

    Delete trade_manager.py

--- trade_manager.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def run_trading_bot():
-    print("[trade_manager] Bot başlatılıyor...")
-    while True:
-        print("[trade_manager] Veri analizi ve al-sat işlemleri yapılıyor...")
-        time.sleep(60)
